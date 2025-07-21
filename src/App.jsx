02ecabbdde60cb6a6966b2ded8b78c953de656df
ArcaneLaboratory <joseph.l.mackelprang@gmail.com>
2025-07-20 20:16:07 -0600

    okay but actually fr this time the link works trust me bro

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,7 @@ function MainApp() {
       <main id="flexcontainer">
         <div class="gamecontainer">
           <h2>Checkers</h2>
-          <a href="https://arcanelaboratory.github.io/src/checkers.html">
+          <a href="https://arcanelaboratory.github.io/checkers.html">
             <img className="gamePreview" src={checkersLogo} alt="Game Preview"  />
           </a>
         </div>
